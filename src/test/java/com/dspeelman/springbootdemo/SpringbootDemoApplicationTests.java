e2d790acad75d455b131044f97cb8abc97ade9af
Andrew Speelman <as7xf@mst.edu>
2025-11-09 16:50:16 -0500

    init

---
+++ src/test/java/com/dspeelman/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dspeelman.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
